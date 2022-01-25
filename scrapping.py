b0fa2c7165d1d611c2830e228f134581a1933892
ychu19 <chu.yuanning@gmail.com>
2022-01-24 19:34:24 -0600

    data exploration (with fenty)

--- scrapping.py
+++ scrapping.py
@@ -8,6 +8,9 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities # dealing with pop-up permissions
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class scrapping_foundations():
     
@@ -25,11 +28,12 @@ class scrapping_foundations():
         self.data = pd.DataFrame(cols)
         self.list_of_revies = []
     
-    def set_up_driver(self, url, geoBlocked = True, first_scroll: int = 2000, second_scroll: int = 3000):
-        if geoBlocked:
+    def set_up_driver(self, url, popup_blocked = True, first_scroll: int = 2000, second_scroll: int = 3000):
+        if popup_blocked:
             geoBlocked = webdriver.FirefoxOptions()
             geoBlocked.set_preference("geo.prompt.testing", True)
             geoBlocked.set_preference("geo.prompt.testing.allow", False)
+            geoBlocked.set_preference("dom.push.enabled", False)
             self.driver = webdriver.Firefox(options=geoBlocked)
         else:
             self.driver = webdriver.Firefox()
@@ -38,13 +42,21 @@ class scrapping_foundations():
         sleep(3)
         self.driver.execute_script(f"window.scrollTo(0, {first_scroll})") 
         sleep(2)
-        self.driver.execute_script(f"window.scrollTo(0, {second_scroll})") 
-        self.list_of_reviews = self.driver.find_elements_by_css_selector('div[data-comp="Review StyledComponent BaseComponent "]')
+        self.driver.execute_script(f"window.scrollTo(0, {second_scroll})")
+        if self.driver.find_elements(By.CSS_SELECTOR, 'ul[data-comp = "Pagination StyledComponent BaseComponent "]') != []:
+            self.list_of_reviews = self.driver.find_elements(By.CSS_SELECTOR, 'div[data-comp="Review StyledComponent BaseComponent "]')
+        else:
+            self.list_of_reviews = []
+        # self.driver.find_elements_by_css_selector('div[data-comp="Review StyledComponent BaseComponent "]')
         # return list_of_reviews
 
     def find_end_page(self):
-        return int(self.driver.find_element_by_css_selector('ul[data-comp = "Pagination StyledComponent BaseComponent "] :nth-child(8)').text)
-
+        page = self.driver.find_element(By.CSS_SELECTOR, 'ul[data-comp="Pagination StyledComponent BaseComponent "]')
+        all_children = page.find_elements(By.CSS_SELECTOR, 'li')
+        return int(all_children[-2].text)
+        # if self.driver.find_elements(By.CSS_SELECTOR, 'ul[data-comp = "Pagination StyledComponent BaseComponent "] :nth-child(8)') != []:
+        #     return int(self.driver.find_element(By.CSS_SELECTOR, 'ul[data-comp = "Pagination StyledComponent BaseComponent "] :nth-child(8)').text)
+        
     def append_new_data(self):
         shade = ""
         review_subject = ""
@@ -53,28 +65,28 @@ class scrapping_foundations():
         rating = int()
         for i in self.list_of_reviews:
             
-            if i.find_elements_by_css_selector('strong[data-at="nickname"]') != []:
-                reviewer_id = i.find_elements_by_css_selector('strong[data-at="nickname"]')[0].text
+            if i.find_elements(By.CSS_SELECTOR, 'strong[data-at="nickname"]') != []:
+                reviewer_id = i.find_elements(By.CSS_SELECTOR,'strong[data-at="nickname"]')[0].text
             else:
                 pass 
             
-            if i.find_elements_by_css_selector('strong[data-at="nickname"]+span') != []:
-                reviewer_feature = i.find_elements_by_css_selector('strong[data-at="nickname"]+span')[0].text
+            if i.find_elements(By.CSS_SELECTOR,'strong[data-at="nickname"]+span') != []:
+                reviewer_feature = i.find_elements(By.CSS_SELECTOR,'strong[data-at="nickname"]+span')[0].text
             else:
                 pass
 
-            if i.find_elements_by_css_selector('div[data-comp="StarRating "]') != []:
-                rating = int(i.find_elements_by_css_selector('div[data-comp="StarRating "]')[0].get_attribute("aria-label")[0])
+            if i.find_elements(By.CSS_SELECTOR,'div[data-comp="StarRating "]') != []:
+                rating = int(i.find_elements(By.CSS_SELECTOR, 'div[data-comp="StarRating "]')[0].get_attribute("aria-label")[0])
             else:
                 pass
 
-            if i.find_elements_by_css_selector('h3') != []:
-                review_subject = i.find_element_by_css_selector('h3').text
+            if i.find_elements(By.CSS_SELECTOR, 'h3') != []:
+                review_subject = i.find_element(By.CSS_SELECTOR, 'h3').text
             else:
                 review_subject = None
 
-            if i.find_elements_by_css_selector('img[src*="https://www.sephora.com/productimages/"]+span') != []:
-                shade = i.find_elements_by_css_selector('img[src*="https://www.sephora.com/productimages/"]+span')[0].text
+            if i.find_elements(By.CSS_SELECTOR, 'img[src*="https://www.sephora.com/productimages/"]+span') != []:
+                shade = i.find_elements(By.CSS_SELECTOR, 'img[src*="https://www.sephora.com/productimages/"]+span')[0].text
             else:
                 shade = None
 
@@ -90,11 +102,16 @@ class scrapping_foundations():
         # return self.data
     
     def clicking_and_scraping(self, starting_page: int = 0):
-        for n in tqdm(range(starting_page, self.find_end_page())):
-            self.list_of_reviews = self.driver.find_elements_by_css_selector('div[data-comp="Review StyledComponent BaseComponent "]')
-            self.append_new_data()
-            self.driver.find_element_by_css_selector('button[aria-label="Next"]').click()
-            sleep(randint(2,6))
+        if self.list_of_reviews != []:
+            end_page = self.find_end_page()
+            for n in tqdm(range(starting_page, end_page)):
+                self.list_of_reviews = self.driver.find_elements(By.CSS_SELECTOR, 'div[data-comp="Review StyledComponent BaseComponent "]')
+                self.append_new_data()
+                self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next"]').click()
+                # self.driver.find_element(By.CSS_SELECTOR, 'button[aria-label="Next"]').click()
+                sleep(randint(2,6))
+        else:
+            self.data = pd.DataFrame()
 
     def close_driver(self):
         self.driver.close()
