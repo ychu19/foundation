2c3f1b7cad820fa53b1ecca22a273340c3e1b127
ychu19 <chu.yuanning@gmail.com>
2022-02-03 17:16:04 -0600

    modularizes feature engineering and generating model metrics

--- training.py
+++ training.py
@@ -9,18 +9,7 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV
 # from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.linear_model import (
-    LinearRegression,
-    LogisticRegression,
-    SGDClassifier
-)
 
-from sklearn.cluster import KMeans
-
-from sklearn.svm import (
-    LinearSVC,
-    SVC
-)
 
 from sklearn.metrics import (
     precision_recall_curve, 
@@ -33,11 +22,7 @@ from sklearn.metrics import (
     confusion_matrix
 )
 
-from sklearn.decomposition import NMF, LatentDirichletAllocation, TruncatedSVD
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-
-import collections
-import re
+import os
 
 # from scipy.stats import pearsonr, interval
 import scipy.stats as st
@@ -49,6 +34,115 @@ class feature_engineering():
 
     def __init__(self, product_name: str) -> None:
         self.data = pd.read_pickle(f'data_full_review_cleaned/{product_name}')
+        self.all_features = [
+            'hair_color', 'eye_color', 'skin_tone', 'skin_type', 
+            'skin_tone_cat', 'finish', 'coverage', 'shade_match', 
+            'gifted', 'days_since_launch_scaled', 'month_of_purchase'
+            ]
+        self.one_hot_features = [
+            'hair_color', 'eye_color', 'skin_tone', 'skin_type'
+        ]
+        self.other_features = [
+            'skin_tone_cat', 'finish', 'coverage', 'shade_match', 
+            'gifted', 'days_since_launch_scaled', 'month_of_purchase'
+        ]
+        self.X = self.data[self.all_features]
+        self.y = self.data['recommended']
+        # perhaps some logging to know if there's class imbalance issues?
+
+    def train_test_split(self, proportion: float = 0.3):
+        self.train_X, self.val_X, self.train_y, self.val_y = train_test_split(self.X, self.y, test_size=proportion, random_state=0)
+
+    def one_hot_encoding(self, cols: list):
+        enc_rest = OneHotEncoder(sparse=False)
+
+        train_X_transform = enc_rest.fit_transform(self.train_X[cols])
+        val_X_transform = enc_rest.transform(self.val_X[cols])
+
+        train_X_transform = pd.DataFrame(train_X_transform)
+        val_X_transform = pd.DataFrame(val_X_transform)
+
+        return train_X_transform, val_X_transform
+
+
+    def cross_one_hot_features(self, one_hot_col1: pd.DataFrame, one_hot_col2: pd.DataFrame, data: pd.DataFrame, col1: str = 'skin_type', col2: str = 'skin_tone'):
+        """
+        given one-hot encoded feature 1 (one_hot_col1) and one-hot encoded feature 2 (one_hot_col1),
+        returns a dataframe with crossed feature b/w one_hot_col1 and one_hot_col2
+        columns of the returned dataframe are named by each of the crossed categories in col1 and col2 as "col1_col2"
+        """
+        total_col1_cat = len(data.groupby([col1],as_index=False).count()) - 1
+        total_col2_cat = len(data.groupby([col2],as_index=False).count()) - 1
+
+        data_cross = pd.DataFrame()
+        
+        i = 0 # col1
+        
+        while i <= total_col1_cat:
+            j = 0 # col2
+            while j <= total_col2_cat:
+                new_cross = one_hot_col1[i] * one_hot_col2[j]
+                new_cross = pd.Series(new_cross)
+                col1_name = data.groupby([col1],as_index=False).count()[col1][i]
+                col2_name = data.groupby([col2],as_index=False).count()[col2][j]
+                data_cross[f'{col1_name}_{col2_name}'] = new_cross
+                j += 1
+            i += 1
+            
+        return data_cross
+    
+    def concat_all_features(
+            self, 
+            train_X_transformed: pd.DataFrame, 
+            val_X_transformed: pd.DataFrame
+        ):
+        """
+        concatenates non-one-hot-encoded features with one-hot-encoded ones
+        """
+        for i in self.other_features:
+            train_X_transformed[i] = self.train_X[i].reset_index(drop = True)
+            val_X_transformed[i] = self.val_X[i].reset_index(drop = True)
+        return train_X_transformed, val_X_transformed
+    
+    def feature_engineering(self):
+        self.train_test_split()
+        
+        self.skin_tone_one_hot_train, self.skin_tone_one_hot_val = self.one_hot_encoding(cols = ['skin_tone'])
+        self.skin_type_one_hot_train, self.skin_type_one_hot_val = self.one_hot_encoding(cols = ['skin_type'])
+        
+        self.tone_type_cross_train = self.cross_one_hot_features(
+            one_hot_col1= self.skin_type_one_hot_train, 
+            one_hot_col2= self.skin_tone_one_hot_train,
+            data = self.train_X
+            )
+        self.tone_type_cross_val = self.cross_one_hot_features(
+            one_hot_col1= self.skin_type_one_hot_val, 
+            one_hot_col2= self.skin_tone_one_hot_val,
+            data = self.val_X
+            )
+
+        self.hair_eye_one_hot_train, self.hair_eye_one_hot_val = self.one_hot_encoding(cols = ['hair_color', 'eye_color'])
+
+        self.train_X_transformed = pd.concat([
+            self.skin_tone_one_hot_train,
+            self.skin_type_one_hot_train,
+            self.tone_type_cross_train,
+            self.hair_eye_one_hot_train
+        ], axis= 1)
+
+        self.val_X_transformed = pd.concat([
+            self.skin_tone_one_hot_val,
+            self.skin_type_one_hot_val,
+            self.tone_type_cross_val,
+            self.hair_eye_one_hot_val
+        ], axis= 1)
+
+        self.train_X_transformed, self.val_X_transformed = self.concat_all_features(self.train_X_transformed, self.val_X_transformed)
+
+        return self.train_X_transformed, self.val_X_transformed
+
+
+
         
 
 
@@ -60,7 +154,7 @@ def plot_countbar(data: pd.DataFrame, product: str, col1: str):
     sns.countplot(x = data[col1], order = data[col1].value_counts().index)
     plt.xticks(rotation=90)
     plt.xlabel(f'Reviewers by {col1}')
-    plt.savefig(f'plots/{product}_reviewers_by_{col1}.jpeg')
+    plt.savefig(f'plots/{product}/reviewers_by_{col1}.jpeg')
 
 
 def plot_diff_in_means(data: pd.DataFrame, product: str, col1: str, col2: str = 'rating'):
@@ -91,7 +185,12 @@ def plot_diff_in_means(data: pd.DataFrame, product: str, col1: str, col2: str = 
     for upper, mean, lower, y in zip(data_agg['upper'], data_agg['mean'], data_agg['lower'], data_agg[col1]):
         plt.plot((lower, mean, upper), (y, y, y), 'b.-')
     plt.yticks(range(len(n)), list(data_agg[col1]))
-    plt.savefig(fname = f'plots/{product}_{col1}_diff_in_means.jpeg')
+    if os.path.isdir(f'plots/{product}'):
+        plt.savefig(fname = f'plots/{product}/{col1}_diff_in_means.jpeg')
+    else:
+        os.makedirs(f'plots/{product}')
+        plt.savefig(fname = f'plots/{product}/{col1}_diff_in_means.jpeg')
+    
 
 
 def plot_cross_tab_heatmap(data: pd.DataFrame, product: str, col1: str, col2: str, col3: str, func: str = 'mean'):
@@ -128,12 +227,18 @@ def plot_cross_tab_heatmap(data: pd.DataFrame, product: str, col1: str, col2: st
             text = im.axes.text(j, i, round(col1_col2_crosstab.iloc[i, j], 2), kw)
             texts.append(text)
     plt.title(product.replace('_', ' '))
-    plt.savefig(fname = f'plots/{product}_{col1}_{col2}_crosstab_heatmap.jpeg')
+    if os.path.isdir(f'plots/{product}'):
+        plt.savefig(fname = f'plots/{product}/{col1}_{col2}_crosstab_heatmap.jpeg')
+    else:
+        os.makedirs(f'plots/{product}')
+        plt.savefig(fname = f'plots/{product}/{col1}_{col2}_crosstab_heatmap.jpeg')
+    
     
 
-
-
-def plot_auc_roc(predict_y, val_y, filename: str = "auc_roc.jpeg"):
+def plot_auc_roc(predict_y, val_y, product: str, model: str, filename: str = "auc_roc"):
+    """
+    given predicted probability (predict_y) and label (val_y), computes AUC-ROC across threshold and saves the figure by product and model
+    """
 
     thresholds = np.linspace(0, 1, 10)
 
@@ -160,10 +265,19 @@ def plot_auc_roc(predict_y, val_y, filename: str = "auc_roc.jpeg"):
     ax3.set_xlabel("False Positive Rate")
     ax3.set_ylabel("True Positive Rate")
     ax3.text(0.55,0.2, 'AUC = {}'.format(roc_auc_))
-    plt.savefig(f'plots/{filename}')
+    
+    if os.path.isdir(f'plots/{product}'):
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
+    else:
+        os.makedirs(f'plots/{product}')
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
 
 
-def plot_recision_recall_f1(predict_y, val_y, filename: str = "precision_recall_f1.jpeg"):
+def plot_recision_recall_f1(predict_y, val_y, product: str, model: str, filename: str = "precision_recall_f1"):
+    """
+    given predicted probability (predict_y) and label (val_y), plots precision, recall, and F1 scores across threshold
+    and saves the figure by product and model
+    """
 
     thresholds = np.linspace(0, 1, 10)
     
@@ -196,25 +310,26 @@ def plot_recision_recall_f1(predict_y, val_y, filename: str = "precision_recall_
     ax2.annotate("{}".format(round(f1_score_at_threshold,3)), (0.5, f1_score_at_threshold))
     ax2.legend()
     ax2.set_xlabel("Thresholds")
-    plt.savefig(f'plots/{filename}')
-
-def cross_one_hot_features(data, col1: str, col2: str, one_hot_col1: pd.DataFrame, one_hot_col2: pd.DataFrame):
-    total_col1_cat = len(data.groupby([col1],as_index=False).count()) 
-    total_col2_cat = len(data.groupby([col2],as_index=False).count()) 
-
-    data_cross = pd.DataFrame()
     
-    i = 0 # col1
+    if os.path.isdir(f'plots/{product}'):
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
+    else:
+        os.makedirs(f'plots/{product}')
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
     
-    while i <= total_col1_cat - 1:
-        j = 0 # col2
-        while j <= total_col2_cat - 1:
-            new_cross = one_hot_col1[:,i] * one_hot_col2[:,j]
-            new_cross = pd.Series(new_cross)
-            col1_name = data.groupby(['skin_tone'],as_index=False).count()['skin_tone'][i]
-            col2_name = data.groupby(['skin_type'],as_index=False).count()['skin_type'][j]
-            data_cross[f'{col1_name}_{col2_name}'] = new_cross
-            j += 1
-        i += 1
-        
-    return data_cross
+
+def plot_predictions_by_scores(predict_y, val_y, product: str, model: str, bins: int = 20, filename: str = 'predictions_by_scores'):
+    """
+    given predicted probability (predict_y) and label (val_y), plots positive and negative instances by predicted probability
+    green: label = 1; red: label = 0
+    """
+    plt.hist(predict_y[val_y == 1.0], bins = bins, color = 'g')
+    plt.hist(predict_y[val_y == 0.0], bins = bins, color = 'r')
+    plt.show()
+
+    if os.path.isdir(f'plots/{product}'):
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
+    else:
+        os.makedirs(f'plots/{product}')
+        plt.savefig(f'plots/{product}/{model}_{filename}.jpeg')
+    
