fd2c0aa8856f8becda4f4c5c59bf3dfb248f4bc7
ychu19 <chu.yuanning@gmail.com>
2022-03-25 18:07:30 -0500

    finalizing

--- scrapping.py
+++ scrapping.py
@@ -2,16 +2,9 @@ import pandas as pd
 from time import sleep # control the crawl rate to avoid hammering the servers with too many requests
 from random import randint
 from tqdm import tqdm
-import re
-import datetime
 
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities # dealing with pop-up permissions
-from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 from google.cloud import storage
 
